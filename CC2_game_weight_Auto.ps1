2e48aabff79b97cd5f894a1a998ac0ce79d80c06
abeck58 <38418219+abeck58@users.noreply.github.com>
2024-12-28 22:36:18 +1000

    Create CC2_game_weight_Auto.ps1
    Code for editing the Carrier Weight limit with the intent of streamlining the process to enable faster .exe file creation when trying to find the correct offset.

---
+++ CC2_game_weight_Auto.ps1
@@ -0,0 +1,35 @@
+[Environment]::CurrentDirectory = (Get-Location -PSProvider FileSystem).ProviderPath
+$ErrorActionPreference = 'Stop'
+
+$file = "carrier_command.exe"
+
+write-host "Hash check bypassed."
+
+write-host Hash check passed for v1.5.2
+
+# Set the Carrier Weight Limit to 2,000,000
+$weight = 2000000
+$weightHex = '{0:X}' -f ([uint32]$weight)
+$bytesarray  = [System.BitConverter]::GetBytes([uint32]$weight)
+
+write-host 'Setting Carrier Weight Limit to 2,000,000'
+write-host 'Debug (Show Hex)', $weightHex
+write-host 'Debug (Show Array)', $bytesarray
+
+$bytes = [System.IO.File]::ReadAllBytes($file)
+
+# Prompt user for the offset
+$weightOffsetInput = Read-Host "Enter the offset for the weight limit (in decimal format)"
+$weightOffset = [int]$weightOffsetInput
+
+# Apply the new weight limit
+$bytes[$weightOffset] = $bytesarray[0]
+$bytes[$weightOffset+1] = $bytesarray[1]
+$bytes[$weightOffset+2] = $bytesarray[2]
+$bytes[$weightOffset+3] = $bytesarray[3]
+
+# Generate the modded file name using the offset with ".exe" added
+$customFileName = "$weightOffsetInput.exe"
+write-host "Writing modded file $customFileName"
+[System.IO.File]::WriteAllBytes($customFileName, $bytes)
+pause
